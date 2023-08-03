962669663e0746c53f022876d6619524f12a00a8
AlexGrig23 <alexgrig.cyber@gmail.com>
2023-08-03 19:18:34 +0300

    Untrack compiled files and pycache

--- twitter/users/models.py
+++ twitter/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=254)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
