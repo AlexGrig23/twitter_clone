b1acd12f76feae8545c70f7e3f9bd32a89bc4afa
AlexGrig23 <alexgrig.cyber@gmail.com>
2023-08-03 20:58:39 +0300

    Create model Post

--- twitter/posts/models.py
+++ twitter/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    content = models.TextField(null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
