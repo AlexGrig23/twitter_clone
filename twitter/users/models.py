fa3251ef91255c84bcf9905e560070604048e478
AlexGrig23 <alexgrig.cyber@gmail.com>
2023-08-04 12:35:56 +0300

    Fixed comments users model

--- twitter/users/models.py
+++ twitter/users/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
-
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    email = models.EmailField(max_length=254)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
 
     date_joined = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
